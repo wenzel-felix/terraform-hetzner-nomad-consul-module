75c2e611ca01806fce2c456b9db6723a44fda584
Felix <felixwenzel0@gmail.com>
2023-01-14 16:04:58 +0100

    changed the setup to provide it as module

---
+++ test/main.tf
@@ -0,0 +1,16 @@
+module "hetzner-nomad-consul" {
+    source = "../"
+    hetzner_token = var.hetzner_token
+}
+
+output "server_info" {
+  value = module.hetzner-nomad-consul.server_info
+}
+
+output "nomad_token" {
+  value = module.hetzner-nomad-consul.nomad_token
+}
+
+output "nomad_address" {
+  value = module.hetzner-nomad-consul.nomad_address
+}
