fb2a340c6f9a75c93db7e348123bd97e8046fc4d
Felix <felixwenzel0@gmail.com>
2023-03-03 23:12:54 +0100

    removed no longer working output in example

--- examples/simple-setup/main.tf
+++ examples/simple-setup/main.tf
@@ -7,10 +7,6 @@ output "server_info" {
   value = module.hetzner-nomad-consul.server_info
 }
 
-output "nomad_token" {
-  value = module.hetzner-nomad-consul.nomad_token
-}
-
 output "nomad_address" {
   value = module.hetzner-nomad-consul.nomad_address
 }
